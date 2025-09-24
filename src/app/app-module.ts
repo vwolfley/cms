04610141a212003b3e9ab93513dc5e712920105f
vern wolfley <vern.wolfley@gmail.com>
2025-09-23 17:07:51 -0700

    initial commit

---
+++ src/app/app-module.ts
@@ -0,0 +1,19 @@
+import { NgModule, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { App } from './app';
+
+@NgModule({
+  declarations: [
+    App
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection()
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
