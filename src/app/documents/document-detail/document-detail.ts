e9fae84890a336dd4aa30756b594e2e5078b3c73
vern wolfley <vern.wolfley@gmail.com>
2025-10-09 13:30:02 -0700

    add document info

--- src/app/documents/document-detail/document-detail.ts
+++ src/app/documents/document-detail/document-detail.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-detail',
   standalone: false,
   templateUrl: './document-detail.html',
-  styleUrl: './document-detail.css'
+  styleUrl: './document-detail.css',
 })
 export class DocumentDetail {
+  @Input() document!: Document;
 
+  documents: Document[] = [];
+
+  constructor() {}
 }
